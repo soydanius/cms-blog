e9bff94b947b8389be06480de4f558ab7dd5358e
Soydan Yuece <soydan.yuece@gmx.de>
2023-09-12 17:09:26 +0200

    Add contentful get all entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react";
+import { createClient } from "contentful";
 import "./App.css";
 
+const client = createClient({
+  space: import.meta.env.VITE_SPACE_ID,
+  accessToken: import.meta.env.VITE_CONTENTFUL_KEY,
+});
+
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    client.getEntries().then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return <div></div>;
 }
 
