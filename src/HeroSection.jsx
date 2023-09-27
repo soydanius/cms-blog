e3bd98d84c36e3ec93f66bea31882fb4e46dcb55
Ajla Saranovic <ge43xed@mytum.de>
2023-09-27 10:14:43 +0200

    hardcode

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -4,54 +4,22 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 import "./HeroSection.css";
 
-const client = createClient({
-  space: import.meta.env.VITE_SPACE_ID,
-  accessToken: import.meta.env.VITE_CONTENTFUL_KEY,
-});
-
 function HeroSection() {
-  const [heroData, setHeroData] = useState(null);
-
-  useEffect(() => {
-    client
-      .getEntries({
-        content_type: "heroSection",
-      })
-      .then((response) => {
-        if (response.items && response.items[0]) {
-          setHeroData(response.items[0].fields);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching Hero Section:", error);
-      });
-  }, []);
-
-  const renderRichText = (richTextField) => {
-    const options = {
-      renderMark: {
-        [MARKS.BOLD]: (text) => <strong>{text}</strong>,
-      },
-      renderNode: {
-        [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
-      },
-    };
-
-    return documentToReactComponents(richTextField, options);
-  };
-
-  if (!heroData) {
-    return null;
-  }
-  const { title, picture, introduction } = heroData;
   return (
     <div className="hero-section">
       <div className="hero-content">
-        <h1>{title}</h1>
+        <h1>Home Adventures for Tiny Tinkers</h1>
 
-        <div className="hero-introduction">{renderRichText(introduction)}</div>
+        <div className="hero-introduction">
+          <p>
+            Where smiles, laughter, and creativity come together. Dive into a
+            world of delightful DIY projects, fun activities, and endless
+            inspiration for kids of all ages. Let's embark on a colorful journey
+            of joy and imagination.
+          </p>
+        </div>
       </div>
-      <img src={picture.fields.file.url} alt={picture.fields.description} />
+      <img src="/hero.png" alt="happy-kid" />
     </div>
   );
 }
