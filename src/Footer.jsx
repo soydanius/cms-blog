455366d251102ef0b8d9295e6b8ba2eba8ccbb12
Piotr Groblewski <groblewskip@gmail.com>
2023-09-27 10:20:14 +0200

    adapting to BE migration

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,52 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { createClient } from "contentful";
+import React from "react";
+//import { createClient } from "contentful";
 import "./Footer.css";
 
-const client = createClient({
-  space: import.meta.env.VITE_SPACE_ID,
-  accessToken: import.meta.env.VITE_CONTENTFUL_KEY,
-});
+// const client = createClient({
+//   space: import.meta.env.VITE_SPACE_ID,
+//   accessToken: import.meta.env.VITE_CONTENTFUL_KEY,
+// });
 
-function Footer() {
-  const [footerData, setFooterData] = useState(null);
+  function Footer() {
+//   const [footerData, setFooterData] = useState(null);
 
-  useEffect(() => {
-    client
-      .getEntries({
-        content_type: "footer",
-      })
-      .then((response) => {
-        if (response.items && response.items[0]) {
-          setFooterData(response.items[0].fields);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching footer data:", error);
-      });
-  }, []);
+//   useEffect(() => {
+//     client
+//       .getEntries({
+//         content_type: "footer",
+//       })
+//       .then((response) => {
+//         if (response.items && response.items[0]) {
+//           setFooterData(response.items[0].fields);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching footer data:", error);
+//       });
+//   }, []);
 
-  if (!footerData) {
-    return null;
-  }
+//   if (!footerData) {
+//     return null;
+//   }
 
-  const { cookiePolicy, dataSecurity, undefinedField, year } = footerData;
+//   const { cookiePolicy, dataSecurity, undefinedField, year } = footerData;
 
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-item">
-          <p>{cookiePolicy}</p>
+          <p><a href=""> Cookie Policy </a></p>
         </div>
         <div className="footer-item">
-          <p>{dataSecurity}</p>
+          <p><a href=""> Data Security</a></p>
         </div>
         <div className="footer-item">
-          <p>{undefinedField}</p>
+          <p> <a href=""> Terms of Use </a></p>
         </div>
         <div className="footer-item">
-          <p>&copy; {year}</p>
+          <p>&copy; 2023</p>
         </div>
       </div>
+    
+      
     </footer>
   );
 }
